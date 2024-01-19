12ba3c961ee3cf1670e1ce2938081cfb32c9c487
ByTum <tum-ok@msn.com>
2024-01-19 10:29:33 +0700

    learn Logging in Users with jwt

--- 4-natours/starter/controllers/authController.js
+++ 4-natours/starter/controllers/authController.js
@@ -1,12 +1,23 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+const signToken = (id) =>
+  jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+//output upstairs code result same below code
+// const signToken = (id) => {
+//   return jwt.sign({ id }, process.env.JWT_SECRET, {
+//     expiresIn: process.env.JWT_EXPIRES_IN,
+//   });
+// };
 
 exports.singup = catchAsync(async (req, res, next) => {
   const newUser = await User.create(req.body);
-  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
+
+  const token = signToken(newUser._id);
 
   res.status(201).json({
     status: 'success',
@@ -21,4 +32,25 @@ exports.singup = catchAsync(async (req, res, next) => {
   //   password: req.body.password,
   //   passwordConfirm: req.body.passwordConfirm,
   // });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+  // 1) check email and password exist
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password!', 400));
+  }
+  // 2) check if user exists && password is correct
+  // find user by email, email = email
+  const user = await User.findOne({ email }).select('+password');
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Incorrect email or password', 401));
+  }
+
+  // 3) if everthing ok, send token to client
+  const token = signToken(user._id);
+  res.status(200).json({
+    status: 'success',
+    token,
+  });
 });
--- 4-natours/starter/controllers/userController.js
+++ 4-natours/starter/controllers/userController.js
@@ -1,9 +1,19 @@
-exports.getAllusers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!',
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getAllusers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+  // send response
+  res.status(200).json({
+    status: 'success',
+    // requestedAt: req.requestTime,
+    results: users.length,
+    data: {
+      users,
+    },
   });
-};
+});
+
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: 'error',
