fdc7f4fd815a3e7c42409872fc666b951120f551
ByTum <tum-ok@msn.com>
2024-01-22 22:17:40 +0700

    learn Setting Security HTTP Headers with helmet package npm

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,24 +1,29 @@
 const express = require('express');
 const morgan = require('morgan');
-const dotenv = require('dotenv');
+// const dotenv = require('dotenv');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
-dotenv.config({ path: './config.env' });
+// dotenv.config({ path: './config.env' });
 
-// 1 GLOBAL MIDDLEWARES
 // console.log(process.env);
+// 1 GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
 
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit requests from same API
 const limiter = rateLimit({
-  max: 3,
+  max: 100,
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, please try again in an hour!',
 });
@@ -26,7 +31,11 @@ const limiter = rateLimit({
 // effect all the route /api
 app.use('/api', limiter);
 
-app.use(express.json());
+// Body parser, reading data from body into req.body
+
+app.use(express.json({ limit: '10kb' })); // <-- limit body 10 kilobyte
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
 // app.use((req, res, next) => {
@@ -34,11 +43,13 @@ app.use(express.static(`${__dirname}/public`));
 //   next();
 // });
 
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   // console.log(req.headers);
   next();
 });
+
 // 3. ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
