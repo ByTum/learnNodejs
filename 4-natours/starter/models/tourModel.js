a2fb67dfb97eb9e5e8a25b84bb3616d05432d226
ByTum <tum-ok@msn.com>
2024-01-23 23:36:26 +0700

    learn Modelling Tour Guides: Child Referencing

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 // const validator = require('validator');
-const User = require('./userModel');
+// const User = require('./userModel');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -100,7 +100,13 @@ const tourSchema = new mongoose.Schema(
       description: String,
       day: Number,
     },
-    guides: Array,
+    // guides: Array,
+    guides: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+      },
+    ],
   },
   {
     toJSON: { virtuals: true },
@@ -119,11 +125,12 @@ tourSchema.pre('save', function (next) {
 });
 
 // work for create document not for update Embedding
-tourSchema.pre('save', async function (next) {
-  const guidesPromises = this.guides.map(async (id) => await User.findById(id));
-  this.guides = await Promise.all(guidesPromises);
-  next();
-});
+// tourSchema.pre('save', async function (next) {
+//   const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+//   this.guides = await Promise.all(guidesPromises);
+//   next();
+// });
+
 // tourSchema.pre('save', function (next) {
 //   console.log('Will save document...');
 //   next();
